e2db98e44515f3087ee7950fa95b1a417ee17bd8
tomaskub <tomasz.kubiak92@gmail.com>
2022-07-31 14:58:10 +0200

    Add view configuration for user already saved and user not saved. Add function to save/remove user from favorites Add responses to UI for user saving / removing

--- gitHubRecrutationChallengeTry2/Controllers/UserDetailViewController.swift
+++ gitHubRecrutationChallengeTry2/Controllers/UserDetailViewController.swift
@@ -19,27 +19,60 @@ class UserDetailViewController: UIViewController {
     
     private var userModel: UserModel?
     var image: UIImage?
+    private var isUserFavorite: Bool?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if let userModel = userModel {
             userNameLabel.text = userModel.userName
+            
+            let results = localRealm.objects(RealmUserModel.self).where({
+                $0.userName == userModel.userName
+            })
+            
+            configureViewFor(_isUserFavorite: !results.isEmpty)
         }
         if let _image = image {
             userAvatarImage.image = _image
         }
+        
         // Do any additional setup after loading the view.
+    }
+    func configureViewFor(_isUserFavorite: Bool){
+        if _isUserFavorite {
+            isUserFavorite = true
+            saveAsFavButton.setTitle("Remove from favorites", for: .normal)
+            saveAsFavButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        } else {
+            isUserFavorite = false
+            saveAsFavButton.setTitle("Save user to favorites", for: .normal)
+            saveAsFavButton.setImage(UIImage(systemName: "star"), for: .normal)
+        }
+    }
+    func setForFavoriteUser() {
+        
     }
     
     func assignUserModel(user: UserModel) {
         self.userModel = user
     }
     @IBAction func saveToFavoriteTouched(_ sender: Any) {
-        if let userModel = userModel {
-            let user = RealmUserModel(value: ["userName": userModel.userName, "avatarUrl": userModel.avatarUrl])
-            try! localRealm.write({
-                localRealm.add(user)
-            })
+        if let userModel = userModel, let _isUserFavorite = isUserFavorite {
+            if !_isUserFavorite {
+                configureViewFor(_isUserFavorite: true)
+                try! localRealm.write({
+                    let user = RealmUserModel(value: ["userName": userModel.userName, "avatarUrl": userModel.avatarUrl])
+                    localRealm.add(user)
+                })
+            } else {
+                configureViewFor(_isUserFavorite: false)
+                try! localRealm.write({
+                    let user = localRealm.objects(RealmUserModel.self).where({
+                        $0.userName == userModel.userName})
+                    localRealm.delete(user)
+                })
+            }
         }
         
     }
